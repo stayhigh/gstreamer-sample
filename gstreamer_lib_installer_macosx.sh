ac8adef393872539abc3f7ca191a03b51a9b7b24
stayhigh <redhero0702@gmail.com>
2016-04-19 14:38:28 +0800

    list note changed

--- gstreamer_lib_installer_macosx.sh
+++ gstreamer_lib_installer_macosx.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 <<LIST
-• gst-plugins-base: an essential exemplary set of elements
-• gst-plugins-good: a set of good-quality plug-ins under LGPL
-• gst-plugins-ugly: a set of good-quality plug-ins that might pose distribution problems
-• gst-plugins-bad: a set of plug-ins that need more quality
-• gst-libav: a set of plug-ins that wrap libav for decoding and encoding
+- gst-plugins-base: an essential exemplary set of elements
+- gst-plugins-good: a set of good-quality plug-ins under LGPL
+- gst-plugins-ugly: a set of good-quality plug-ins that might pose distribution problems
+- gst-plugins-bad: a set of plug-ins that need more quality
+- gst-libav: a set of plug-ins that wrap libav for decoding and encoding
 LIST
 
 brew install gst-plugins-base gst-plugins-good gst-plugins-ugly gst-plugins-bad gst-libav
